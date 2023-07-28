2ebc47a76b7e634692afe163657d41f22c74d0b0
JustBKhere <90069354+justBKhere@users.noreply.github.com>
2023-07-28 05:44:49 +0530

    Battle Bots Data

--- src/routes/gameRoutes.ts
+++ src/routes/gameRoutes.ts
@@ -4,7 +4,7 @@ import express, { Router, Request, Response } from 'express';
 const router: Router = express.Router();
 
 // Register a new user
-router.post('/register', UserController.register);
+//router.post('/register', UserController.register);
 
 
 
